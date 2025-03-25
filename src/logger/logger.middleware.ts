03b46b14c58a4f1f27a56cf8503f8398d298e887
Hossein Mayboudi <smhmayboudi@gmail.com>
2025-03-25 12:44:42 +0330

    feat: add the logger functional middlware

--- src/logger/logger.middleware.ts
+++ src/logger/logger.middleware.ts
@@ -1,12 +1,13 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
+  use(req: Request, res: Response, next: NextFunction) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    console.log({ Request: req });
+    console.log({ LoggerMiddlewareRequest: req });
     next();
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    console.log({ Response: res });
+    console.log({ LoggerMiddlewareResponse: res });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { Request, Response, NextFunction } from 'express';
+
+export function logger(req: Request, res: Response, next: NextFunction) {
+  console.log({ Request: req });
+  next();
+  console.log({ Response: res });
+}
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     abortOnError: false,
   });
+  app.use(logger);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
