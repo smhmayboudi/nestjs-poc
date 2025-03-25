4174b9398806d34978d3451ce88e4fba3372c98a
Hossein Mayboudi <smhmayboudi@gmail.com>
2025-03-25 11:05:40 +0330

    feat: add qs

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,16 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { parse } from 'qs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    new FastifyAdapter({
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
+      querystringParser: (string) => parse(string),
+    }),
     { abortOnError: false },
   );
   await app.listen(process.env.PORT ?? 3000);
